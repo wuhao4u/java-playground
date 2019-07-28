756d8f0b8362be9bf50bd5b87477393b5c2e5d32
wuhao4u <wuhao4u@gmail.com>
2019-07-28 17:38:40 -0400

    finished pluralsight java lang class

---
+++ javaFundamentals/theJavaLanguage/src/Parallellism/ProducerConsumer.java
@@ -0,0 +1,18 @@
+package Parallellism;
+
+public class ProducerConsumer {
+    private static int[] buffer;
+    private static int count;
+
+    static class Producer {
+        void produce() {
+            
+        }
+    }
+
+    static class Consumer {
+        void consume() {
+
+        }
+    }
+}
