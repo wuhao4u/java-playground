30d132c2c8c32331e47ad28e53b2c21b894cfdc0
Howard Wu <wuhao4u@gmail.com>
2022-07-17 07:56:30 -0400

    added modular examples

---
+++ javaTestBed/src/main/java/ModularExamples.java
@@ -0,0 +1,15 @@
+public class ModularExamples {
+    public void positiveModular() {
+        System.out.println(1024 % 10);
+    }
+
+    public void negativeModular() {
+        System.out.println(-1024 % 10);
+    }
+
+    public static void main(String[] args) {
+        ModularExamples modularExamples = new ModularExamples();
+        modularExamples.positiveModular();
+        modularExamples.negativeModular();
+    }
+}
